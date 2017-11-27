901684c442c80d4b28e0f7a72dcf82ea9f0fe3e9
Unknown <damianniemiec9605@gmail.com>
2017-11-27 01:43:32 +0100

    PSWL6 - 001 metody JS

---
+++ lab1/js/punkt 1.js
@@ -0,0 +1,37 @@
+function createButton() {
+    var btn = document.createElement("BUTTON");
+    document.body.appendChild(btn);
+}
+
+function addText() {
+    var t = document.createTextNode("Tekst stworzony przy użyciu createTextNode");      
+    document.body.appendChild(t);
+}
+
+function addBefore(){
+    var newItem = document.createElement("LI");
+    var textNode = document.createTextNode("Nowy element");
+    newItem.appendChild(textNode);
+    
+    var list = document.getElementById("lista");
+    list.insertBefore(newItem, list.childNodes[0]);
+}
+
+function replace(){
+    var newItem = document.createElement("LI");
+    var textNode = document.createTextNode("zamieniony element");
+    newItem.appendChild(textNode);
+    
+    var list = document.getElementById("lista");
+    list.replaceChild(newItem, list.childNodes[0]);
+}
+
+function remove(){
+    var list = document.getElementById("lista");
+    list.removeChild(list.lastElementChild);
+}
+
+function getParentNode(){
+    var x = document.getElementById("elem").parentNode.nodeName;
+    document.getElementById("wynik").innerHTML = x;
+}
