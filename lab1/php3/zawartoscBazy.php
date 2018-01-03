223e346dd94f9261f975976b5174db0267d58232
Andrzej Dabrowski <andrzej.dabrowski94@gmail.com>
2018-01-03 17:57:04 +0100

    PSWL8-poprawione wyswietlanie

--- lab1/php3/zawartoscBazy.php
+++ lab1/php3/zawartoscBazy.php
@@ -6,15 +6,19 @@
 </head>
     <body>
         <?php
-            if(!($database = mysqli_connect("localhost", "root","root","php2")))
+            if(!($database = mysqli_connect("localhost", "root","","php2")))
                 die("Nie można połączyć się z bazą </body></html>");
             if(!($result = mysqli_query($database, "SELECT * FROM uzytkownicy")))
+            {
                 print("<p>Nieudane wykonanie zapytania</p>");
-                die(mysqli_error($database) . "</body></hyml>")
+                die(mysqli_error($database) . "</body> ooooo</hyml>");
+            }
         ?>
         <table>
         <caption>Wyniki zapytania</caption>
             <?php
+             
+           // var_dump($row);
                 while($row = mysqli_fetch_row($result))
                 {
                     print("<tr>");
